0ac55c2815b6ea9c213c117d5db444fe24ab3267
scionwest <scionwest@gmail.com>
2023-12-17 10:04:52 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Johnathon Sullinger on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
