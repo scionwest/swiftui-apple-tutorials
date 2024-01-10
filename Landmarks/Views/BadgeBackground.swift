715954a781856c226a67c5228cc4c0aa934b2a07
scionwest <scionwest@gmail.com>
2024-01-09 19:45:07 -0700

    Drawing Paths and Shapes. Completed Section 2, Step 4.

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,37 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Johnathon Sullinger on 1/9/24.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: width * 0.20
+                )
+            )
+            
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(
+                    to: CGPoint(
+                        x: width * segment.line.x,
+                        y: height * segment.line.y
+                    )
+                )
+            }
+        }
+        .fill(.black)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
